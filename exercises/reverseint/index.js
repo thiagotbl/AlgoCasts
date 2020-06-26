cbb3256b62286156c48872d19925281e272a5716
Thiago Lopes <lopestbl@gmail.com>
2020-06-26 08:35:36 -0300

    Add reverseInt solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const sign = n < 0 ? -1 : 1
+    const reversed = Math.abs(n).toString().split('').reverse().join('')
+
+    return sign * reversed
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     const digits = String(n).split('')
+//     const firstDigit = digits[0]
+//     const reversedDigits = digits.splice(1).reverse().join('')
+
+//     let response
+
+//     if (firstDigit === '-') {
+//         response = firstDigit + reversedDigits
+//     } else {
+//         response = reversedDigits + firstDigit
+//     }
+
+//     return Number(response)
+// }
