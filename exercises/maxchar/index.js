6f6febbee151bb7ea7e56ba00f7fabb40e42f7a9
Thiago Lopes <lopestbl@gmail.com>
2020-06-27 09:41:18 -0300

    Add maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = str.split('')
+    const charsMap = {}
+    let mostCommonChar = str[0]
+
+    chars.forEach(c => {
+        if (charsMap[c]) {
+            charsMap[c] += 1
+        } else {
+            charsMap[c] = 1
+        }
+    })
+
+    Object.keys(charsMap).forEach(k => {
+        if (charsMap[k] > charsMap[mostCommonChar]) {
+            mostCommonChar = k
+        }
+    })
+
+    return mostCommonChar
+}
 
 module.exports = maxChar;
