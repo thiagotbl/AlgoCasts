66ef1ffb9d86342ee3cc178d6d88704260a17278
Thiago Lopes <lopestbl@gmail.com>
2020-06-27 17:38:35 -0300

    Add pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,24 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   ' 1 -> 1 + (1 - 1) = 1
+//       '  ###  ' 2 -> 2 + (2 - 1) = 3
+//       ' ##### ' 3 -> 3 + (3 - 1) = 5
+//       '#######' 4 -> 4 + (4 - 1) = 7
+//                      i + (i - 1) = 2i -1
 
-function pyramid(n) {}
+function pyramid(n) {
+    const rowSize = 2 * n - 1
+
+    for (let i = 1; i <= n; i++) {
+        const numberOfPounds = 2 * i - 1
+        const numberOfSpaces = (rowSize - numberOfPounds) / 2
+        const pounds = Array(numberOfPounds).fill('#').join('')
+        const spaces = Array(numberOfSpaces).fill(' ').join('')
+
+        console.log(spaces + pounds + spaces)
+    }
+}
 
 module.exports = pyramid;
