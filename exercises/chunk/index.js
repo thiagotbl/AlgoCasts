0387cde26cb8083b47a87ca14d4cf568298e4649
Thiago Lopes <lopestbl@gmail.com>
2020-06-27 13:26:51 -0300

    Add chunk solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = []
+    let i = 0
+
+    while (i < array.length) {
+        chunked.push(array.slice(i, i + size))
+
+        i += size
+    }
+
+    return chunked
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     const response = []
+//     let chunk = []
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (chunk.length === size) {
+//             response.push(chunk)
+//             chunk = []
+//         }
+
+//         chunk.push(array[i])
+//     }
+
+//     response.push(chunk)
+
+//     return response
+// }
--- exercises/chunk/test.js
+++ exercises/chunk/test.js
@@ -11,6 +11,13 @@ test('chunk divides an array of 10 elements with chunk size 2', () => {
   expect(chunked).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
 });
 
+test('chunk divides an array of 10 elements with chunk size 10', () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const chunked = chunk(arr, 10);
+
+  expect(chunked).toEqual([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
+});
+
 test('chunk divides an array of 3 elements with chunk size 1', () => {
   const arr = [1, 2, 3];
   const chunked = chunk(arr, 1);
