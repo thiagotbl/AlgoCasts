037428b1f127c9ffa5ec1cf0d017a4d9592e578c
Thiago Lopes <lopestbl@gmail.com>
2020-06-28 15:08:10 -0300

    Add stack implementation

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+    constructor() {
+        this.data = []
+    }
+
+    push(element) {
+        this.data.unshift(element)
+    }
+
+    pop() {
+        return this.data.shift()
+    }
+
+    peek() {
+        return this.data[0]
+    }
+}
 
 module.exports = Stack;
