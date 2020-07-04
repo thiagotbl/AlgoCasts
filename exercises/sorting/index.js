71e180c7cde36fe78d1b62c41e772cadbb6847c5
Thiago Lopes <lopestbl@gmail.com>
2020-07-04 11:32:16 -0300

    Add selectionSort implementation

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,23 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j
+            }
+        }
+
+        if (indexOfMin !== i) {
+            const aux = arr[i]
+            arr[i] = arr[indexOfMin]
+            arr[indexOfMin] = aux
+        }
+    }
+
+    return arr
 }
 
 function mergeSort(arr) {
